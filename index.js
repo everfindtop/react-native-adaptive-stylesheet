efa087d7fa183759806a02a520611eaea531731d
tbingooo <bingooo.tang@gmail.com>
2018-09-06 11:31:00 +0800

    feat: first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// import { Dimensions, PixelRatio } from 'react-native';
+
+// const GUIDELINE_BASE_WIDTH = 375;
+// const { width } = Dimensions.get('window');
+
+// export const scale = size => width / GUIDELINE_BASE_WIDTH * size;
